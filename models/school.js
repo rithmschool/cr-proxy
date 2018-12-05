609b45ae72440b493245670fb8b274d2c0b10ec6
Silas Burger <ybsilas@gmail.com>
2018-12-05 11:35:48 -0800

    Adding boiler plate for schools routes

---
+++ models/school.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = new express.Router();
+const axios = require('axios');
+
+/**
+ * GET / Blogs 
+ * returns list of blogs
+ * (consider pagination)
+ */
+router.get('/', async function(req, res, next) {
+
+
+});
